cc40cdb6a4a969e91c034b74db4ab1d565d2b566
Henry Wu <hhw67865@gmail.com>
2022-09-23 14:51:37 -0400

    commit tree ef9696da56dacc0a61b0eea3c68d23495981b8ba

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    const cards = [];
+    for (let i = 0; i<array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown(n) {
+    while (n>-1) {
+        console.log(n);
+        n--;
+    }
+}
